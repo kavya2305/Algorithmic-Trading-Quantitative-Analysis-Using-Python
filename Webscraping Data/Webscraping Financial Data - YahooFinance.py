a0e0e50732fdd39347f3249afa906dba53039297
Kavya Gopalakrishnan <kavya.g2305@gmail.com>
2025-08-03 21:03:03 +0530

    Basic Statistics with Data

---
+++ Webscraping Data/Webscraping Financial Data - YahooFinance.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  3 14:54:00 2025
+
+@author: kavya
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+income_statement_dict = {}
+balance_sheet_dict = {}
+cashflow_st_dict = {}
