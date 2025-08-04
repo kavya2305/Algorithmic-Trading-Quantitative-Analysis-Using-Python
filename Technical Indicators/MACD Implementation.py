e1566a483bd4956d09d6b610d567b277c45b54da
Kavya Gopalakrishnan <kavya.g2305@gmail.com>
2025-08-04 20:43:11 +0530

    Technical Indicator Implementations

---
+++ Technical Indicators/MACD Implementation.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  4 19:51:07 2025
+
+@author: kavya
+"""
+
+import yfinance as yf
+
+tickers = ["AMZN","GOOG","MSFT"]
+ohlcv_data={}
+
+for ticker in tickers:
+    temp = yf.download(ticker,period="1mo",interval="15m")
+    temp.dropna(how="any",inplace=True)
+    ohlcv_data[ticker] = temp
+   
+def MACD(DF,a=12,b=26,c=9):
+    df=DF.copy()
+    df["ma_fast"]=df["Close"].ewm(span=a,min_periods=a).mean()
+    df["ma_slow"]=df["Close"].ewm(span=a,min_periods=a).mean()
+    df["macd"] = df["ma_fast"] - df["ma_slow"]
+    df["signal"] = df["macd"].ewm(span = c,min_periods = c).mean()
+    return df.loc[:,["macd","signal"]]
+
+for ticker in ohlcv_data:
+    ohlcv_data[ticker][["MACD","SIGNAL"]] = MACD(ohlcv_data[ticker])
+    
